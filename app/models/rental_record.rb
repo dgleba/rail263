d0bab0697fa08ae6caf9b4c13ed5263e2b357b81
David Gleba <dgleba@gmail.com>
2016-06-12 17:23:42 -0400

    add to_s method show text rather than record object number like #Vehicle0x007f343b3f2890

--- app/models/rental_record.rb
+++ app/models/rental_record.rb
@@ -5,5 +5,14 @@ class RentalRecord < ActiveRecord::Base
   has_many :passengers
   accepts_nested_attributes_for :passengers, reject_if: :all_blank, allow_destroy: true
   
+  
+  def to_s
+    # show columns contents, not record object like: 	#<Vehicle:0x007f343b3f2890>	2016-06-08	Details	Edit	Delete
+    # http://stackoverflow.com/questions/4829909/how-do-i-print-out-the-contents-of-an-object-in-rails-for-easy-debugging
+    #"Name:#{self.name} Age:#{self.age} Weight: #{self.weight}"
+    "##{id}, start:#{start_date}"
+  end
+
+  
 end
 
