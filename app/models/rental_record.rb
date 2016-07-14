388a4452159d8fb3aade8b2b5c49426c108e3c1c
dgleba <dgleba@gmail.com>
2016-07-14 06:08:06 -0400

    add to_s

--- app/models/rental_record.rb
+++ app/models/rental_record.rb
@@ -1,15 +1,16 @@
 class RentalRecord < ActiveRecord::Base
   belongs_to :customer
   belongs_to :vehicle
-  
+
   has_many :passengers
   has_many :pasenger_lists, :through => :passengers, :class_name => 'PasengerList'
 
   accepts_nested_attributes_for :pasenger_lists
   accepts_nested_attributes_for :passengers,  allow_destroy: true
   #accepts_nested_attributes_for :passengers, reject_if: :all_blank, allow_destroy: true
-  
-  
+
+  default_scope {order('id DESC')}
+
   def to_s
     # show columns contents, not record object like: 	#<Vehicle:0x007f343b3f2890>	2016-06-08	Details	Edit	Delete
     # http://stackoverflow.com/questions/4829909/how-do-i-print-out-the-contents-of-an-object-in-rails-for-easy-debugging
@@ -17,5 +18,5 @@ class RentalRecord < ActiveRecord::Base
     "##{id}, start:#{start_date}"
   end
 
-  
+
 end
