fd0959e4fdecc2cf114ccc85dd2b7d0726103ec3
dgleba <dgleba@gmail.com>
2016-08-09 10:02:08 -0400

    1. Added to search items in index header. 2. change cancel to back.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722190232) do
+ActiveRecord::Schema.define(version: 20160809135309) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
