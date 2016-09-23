ae94d4628c9d66b27285727302fef1cc8ab3ddb1
David Gleba <dgleba@gmail.com>
2016-09-22 20:53:59 -0400

    2016-09-22

--- app/helpers/customer_helper.rb
+++ app/helpers/customer_helper.rb
@@ -6,12 +6,13 @@ module CustomerHelper
     attributes_to_exclude = [
       "created_at",
       "updated_at",
+      "discount",
       "id"     
     ]
     Customer.column_names.each do |column_name|
       most_attributes << column_name unless column_name.in?(attributes_to_exclude)
     end
-    most_attributes.join("_or_") + "_cont_any"
+    most_attributes.join("_or_") + "_cont_all"
   end
  
    
