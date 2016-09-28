5d75fb31ed5387638e34fc2035b43b5b8e49db6f
dgleba <dgleba@gmail.com>
2016-09-28 13:15:33 -0400

    env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# ENV['RAILS_ENV'] = 'production'
+# ENV['RAILS_ENV'] ||= 'development'
+
 # Initialize the Rails application.
 Rails.application.initialize!
