b79c8cbdaed0e9e8d28c9c542c4034dedb3fb7ce
albe <albe@pmdsdata9.local>
2016-10-29 14:49:02 -0400

    misc

--- hook.sh
+++ hook.sh
@@ -2,3 +2,13 @@
 
   cp -rfp util/hooks/* .git/hooks
  
+  mtimestore -s
+    sleep 2
+
+  cat .mtimes |  ruby util/mtimeh.rb  > mtimeh.txt
+  sort -r mtimeh.txt   > mtimehs.txt
+    sleep 1
+
+  git add .mtimes mtimeh.txt mtimehs.txt
+ 
+ 
